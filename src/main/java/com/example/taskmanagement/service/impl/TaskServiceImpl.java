0606902056754715d926260abd788b4dee31ada3
ShuMAhd <shuman@zykaika.ru>
2024-02-25 00:03:08 +0800

    first commit

---
+++ src/main/java/com/example/taskmanagement/service/impl/TaskServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.taskmanagement.service.impl;
+
+import com.example.taskmanagement.model.Task;
+import com.example.taskmanagement.service.TaskService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+  private final List<Task> tasks = new ArrayList<>();
+
+  @Override
+  public List<Task> getAllTasks() {
+    return tasks;
+  }
+
+  @Override
+  public Task createTask(Task task) {
+    return null;
+  }
+
+  @Override
+  public Task getTaskById(Long id) {
+    return null;
+  }
+
+  @Override
+  public Task updateTask(Long id, Task task) {
+    return null;
+  }
+
+  @Override
+  public void deleteTask(Long id) {
+
+  }
+
+  @Override
+  public void addTask(Task task) {
+    tasks.add(task);
+  }
+}
